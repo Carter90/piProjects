7ab2dbedf32dc67e2ccd32c63e02897dbd8c5059
Carter Frost <carter90@gmaill.com>
2018-09-12 20:04:03 -0700

    Removed old files that have been merged or no longer valid

--- relay/scripts.sh
+++
@@ -1,6 +0,0 @@
-sed -i '1s/.*/T/' $state
-sed -i '1s/.*/F/' $state
-
-sed -i '2s/.*/T/' /home/carter/Dropbox/CISCSClasses/fun/relay/state.txt
-
-sh -c "state=/home/carter/Dropbox/CISCSClasses/fun/relay/state.txt; [ `sed '1q;d' $state` = "T" ] && sed -i '1s/.*/F/' $state || sed -i '1s/.*/T/' $state"
