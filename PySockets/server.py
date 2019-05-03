1ae20e7bfc1e5f1df4cddc4a7c2d9b48fdf90d85
Carter Frost <carter90@gmail.com>
2019-05-03 08:54:02 -0700

    server.py: Added Todos, removed hardcoded values, Major refactor of the Buttons class, maintained its behavior, Creation of skelliton for IRControl, Cleaned up threads, Moved main code into process, passes the config file path, PEP8 compliance, Typo fixes, added 2 push buttons, & Fixed MessageParser call

--- PySockets/server.py
+++ PySockets/server.py
@@ -11,7 +11,10 @@ import RPi.GPIO as GPIO
 import urllib2
 import datetime
 
-# TODO: look at workspace.txt for any projects to intergrate
+connection = None
+sock = None
+signal_catcher = None
+# TODO: look at workspace.txt for any projects to integrate
 
 # https://stackoverflow.com/questions/18499497/how-to-process-sigterm-signal-gracefully
 # Thanks to Mayank Jaiswal for help with signal handling
@@ -22,9 +25,11 @@ class SignalCatcher:
         signal.signal(signal.SIGTERM, self.exit_called)  # Termination signal #15
 
     def exit_called(self, signum, frame):
+        global connection
         try:
             print("Got signal:", signum, " from", frame)
             connection.close()  # Clean up the connection, possible scope problem
+            sock.close()  # Local Listener
             print("Cleaning up ... Exiting")
         except NameError:
             print("No prior connections made ... Exiting")
@@ -38,31 +43,31 @@ class RelayControl:
         import RPi.GPIO as GPIO
         GPIO.setmode(GPIO.BCM)
         self.GPIO = GPIO
-        self._pinList = [2, 3, 4, 17, 27, 22, 10, 9]
-        self._buttonList = [ 16, 20 ]
+        self._pinList = [2, 3, 4, 17, 27, 23, 10, 9]
+        self._buttonList = [ 16, 26 ] # TODO: read from server.confg
         # GPIO pins on the Pi so the first relay is at GPIO pin 2, the 4th is at GPIO pin 17
-        self._relaystate = [False] * 8
+        self._relay_state = [False] * 8
         for index in range(len(self._pinList)):  # turn off all the relays
             print("GPIO.setup pin:" + str(self._pinList[index]))
             GPIO.setup(self._pinList[index], GPIO.OUT)
             GPIO.output(self._pinList[index], GPIO.HIGH)
         for pin in self._buttonList:  # set button pins to 3.3v
             GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-            GPIO.add_event_detect(pin, GPIO.RISING, callback=self.button_pressed, bouncetime=1000)
+            GPIO.add_event_detect(pin, GPIO.RISING, callback=self.button_pressed, bouncetime=1500) #was GPIO.FALLING
     
     def _change_relay_state(self, relay_num, new_state):
         print("Setting relay #", relay_num, "to", new_state)
         
         if new_state:
             self.GPIO.output(self._pinList[int(relay_num)-1], self.GPIO.LOW)  # on
-            self._relaystate[int(relay_num)-1] = True
+            self._relay_state[int(relay_num)-1] = True
         else: 
             self.GPIO.output(self._pinList[int(relay_num)-1], self.GPIO.HIGH)  # off
-            self._relaystate[int(relay_num)-1] = False
+            self._relay_state[int(relay_num)-1] = False
     
     def invert(self, relay_num):
         print("invert called")
-        self._change_relay_state(relay_num, not self._relaystate[relay_num-1])
+        self._change_relay_state(relay_num, not self.state(relay_num))
 
     def on(self, relay_num):
         self._change_relay_state(relay_num, True)
@@ -74,25 +79,25 @@ class RelayControl:
         for index in range(len(self._pinList)):
             self._change_relay_state(index+1, False)
 
-    def state(self,relay_num):
-        return self._relaystate[int(relay_num)-1]
+    def state(self, relay_num):
+        return self._relay_state[int(relay_num)-1]
 
     def states(self):
-        return self._relaystate
+        return self._relay_state
         
     def button_pressed(self, channel):
         print("Button Pressed", channel)
-        if channel == 20:
-            self.invert(1)
-        elif channel == 16: 
-            self.invert(7)
+        if channel == 26:
+            pass #self.invert(8)
+        elif channel == 16: #TODO: invistigate why GPIO 16 triggers 26 rising, either coding error or physical wiring 
+            pass #self.invert(2)
     
     def __del__(self):
         print("Cleaning up GPIO")
         GPIO.cleanup()
 
         
-class IRControl: # for air conditioner controll, 
+class IRControl: # for air conditioner control,
     '''@note had problems with IR on the pi3 b+ works on pi2 b+ have to get that working first'''
     def __init__(self):  
         pass
@@ -110,14 +115,14 @@ class MessageParser:  # TODO: Complete
         self._validMessage = {'on':self._relay_control.on,
                               'off':self._relay_control.off,
                               'invert':self._relay_control.invert}
-        self._relayPattern = re.compile("^([Rr][1-8])$")  # possiblity have a range spcified in a config
-        # TODO: read the config file for fan names and create dictary to look up corisponding
+        self._relayPattern = re.compile("^([Rr][1-8])$")  # possibility have a range specified in a config
+        # TODO: read the config file for fan names and create dictionary to look up corresponding
         # TODO: self._ fan R1 r1 light red [1]
         # TODO: toggle state with other if on and turn on too?
         # TODO: like red: turns off the white light if on and turns r7 on,
-        # TODO: line fans: controlls both the r2 & r8,
+        # TODO: line fans: controls both the r2 & r8,
 
-    def decrypt():  # TODO: do maybie refactor to verify too
+    def decrypt():  # TODO: do maybe refactor to verify too
         pass
 
     def parse(self, message):
@@ -144,7 +149,7 @@ class DropboxListener:
 
     def run(self):     
         while self._updated_page[8] != 'stop':
-            ''' if I have to remotly kill the DropboxListener, 
+            ''' if I have to remotely kill the DropboxListener, 
                 will not start back up until reboot, 
                 TODO: turning all the relays off and killing the process
                 TODO: check button state
@@ -169,9 +174,7 @@ class DropboxListener:
 class Config:
     def __init__(self):  # TODO: move all the ConfigParser code here
         self.buttons = dict()
-        
         pass
-    
 
 class Buttons:
     def __init__(self, relay_control):
@@ -186,7 +189,7 @@ class Buttons:
             python3 = False
             import ConfigParser  # 2
             config = ConfigParser.ConfigParser()  # 2
-        config.read('/home/carter/relay/PySockets/server.confg')  # TODO: change path baised on location
+        config.read('/home/carter/relay/PySockets/server.confg')  # TODO: change path based on location
         # TODO: Strip whitespace maybe needed
 
         if config.has_section('Buttons'):
@@ -205,7 +208,7 @@ class Buttons:
                 # print("seen arp from: ",pkt[ARP].hwsrc)
                 if pkt[ARP].hwsrc in self.buttons:
                     print(self.buttons[pkt[ARP].hwsrc]) # prints mac address and desired action
-                    # TODO: possiblty have a field in the config for the action send to the parcer
+                    # TODO: possibly have a field in the config for the action send to the parcer
                     self._relay_control.invert(2) # TODO: look at self.buttons[pkt[ARP].hwsrc][1] for action
                     self._relay_control.invert(8)
 
@@ -215,12 +218,14 @@ class Buttons:
 
 #Some of the TCP/IP Client and Server code from https://pymotw.com/2/socket/tcp.html
 class LocalServer:
-    def __init__(self, relay_control): 
+    message_parser = None
+    def __init__(self, relay_control):
+        global sock
         # Create a TCP/IP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+        
         # Bind the socket to the port
-        server_address = ('', 19090) #'' will use localhost, port #
+        server_address = ('', 19090)  # '' will use localhost, port #
         self.sock.bind(server_address)
     
         # Listen for incoming connections
@@ -233,23 +238,25 @@ class LocalServer:
         message_parser.parse("true r2")
         message_parser.parse("off fans")
         ''' #end testing
+        self.sock.close()  # what? ****************************************************************************************
         
     def run(self):
         global signal_catcher
-        while signal_catcher.running: # and not (sys.stdin in select.select([sys.stdin], [], [], 0)[0]):
-            connection, client_address = self.sock.accept() # Wait for a connection
+        global connection
+        while signal_catcher.running:  # and not (sys.stdin in select.select([sys.stdin], [], [], 0)[0]):
+            connection, client_address = self.sock.accept()  # Wait for a connection
             try:
-                hasData=True
-                while hasData:
-                    data = connection.recv(16) # Receives the data in small chunks and retransmit it
+                has_data=True
+                while has_data:
+                    data = connection.recv(16)  # Receives the data in small chunks and retransmit it
                     if data:
-                        print("Local Server Data Receved:",data)
-                        message_parser(data) #TODO: concatinate data before sending to parser
-                        connection.sendall(data) #TODO: only if sucessfull send reply for security
+                        print("Local Server Data Received:", data)
+                        message_parser.parse(data)  # TODO: concatenate data before sending to parser
+                        connection.sendall(data)  # TODO: only if successful send reply for security
                     else:
-                        hasData = False
+                        has_data = False
             except:
-                print("Something Bad Happend")
+                print("Something Bad Happened")
                 connection.close()  # Clean up the connection
         connection.close()
 
@@ -259,10 +266,10 @@ def process(config):
     signal_catcher = SignalCatcher()                     # calls init
     relay_control = RelayControl()                        # calls init
     dropbox_listener = DropboxListener(relay_control)     # calls init
-    dropbox_listener._relay_control = relay_control     # TODO: test if nessesary
+    dropbox_listener._relay_control = relay_control     # TODO: test if necessary
     local_server = LocalServer(relay_control)
     buttons = Buttons(relay_control)
-    buttons._relay_control = relay_control  # TODO: test if nessesary
+    buttons._relay_control = relay_control  # TODO: test if necessary
 
     # Dropbox File Change Listener
     dropbox__thread = Thread(target = dropbox_listener.run)
@@ -276,7 +283,7 @@ def process(config):
     dash_thread.start()  # starts the arp packet sniffer looking for the dash button presses
     local_server_thread.start()
     time.sleep(5)
-    if (dropbox__thread.isAlive() and dash_thread.isAlive() and local_server_thread.isAlive()):
+    if dropbox__thread.isAlive() and dash_thread.isAlive() and local_server_thread.isAlive():
         print('All Threads Started')
     else:
         print("Dropbox Started:",dropbox__thread.isAlive())
@@ -285,7 +292,7 @@ def process(config):
     while signal_catcher.running: 
         time.sleep(2)  # so termination is not immediate
         pass  # keep running until receve kill signal
-    connection.close()        # not sure if I have scope as this is the local server's
+    #connection.close()        # not sure if I have scope as this is the local server's
     dash_thread.join()         # close the Dash Button thread
     dropbox__thread.join()     # close the Dropbox thread
     local_server_thread.join()     # close the local server thread
